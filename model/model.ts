6e8ea27c841af0b2cec539e7f08631fef3d4988a
Hm255 <hamza.moosa@outlook.com>
2025-02-25 20:33:17 +0000

    listening needs some more work

--- model/model.ts
+++ model/model.ts
@@ -1,11 +1,28 @@
 import db from '../db/connection.js';
 
+// models/carModel.ts
+
 interface Car {
-  id: number;
-  make: string;
-  model: string;
-  year: number;
+  registration_number: string; // character(7)
+  make_id: number; // integer
+  model_id: number; // integer
+  color: string; // character varying(30)
+  engine_size: number; // integer
+  year_of_manufacture: number; // integer
+  date_of_manufacture: Date; // date
+  co2_emissions: number; // integer
+  tax_due_date: Date; // date
+  date_of_last_v5c_issued: Date; // date
+  first_used_date: Date; // date
+  marked_for_export: boolean; // boolean
+  has_outstanding_recall: boolean; // boolean
+  type_approval: string; // character varying(10)
+  fuel_type_id: number; // integer
+  tax_status_id: number; // integer
+  wheel_plan_id: number; // integer
+  power_output: number; // integer
 }
+
 
 export const fetchCars = async (): Promise<Car[]> => {
   try {
