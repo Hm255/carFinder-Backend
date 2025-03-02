b64f68be2b59e7544dffff95311c47718b83790a
Hm255 <hamza.moosa@outlook.com>
2025-03-02 15:30:54 +0000

    frontend serving attempted

--- app.ts
+++ app.ts
@@ -1,35 +1,49 @@
-import express, {Request, Response, NextFunction} from 'express';
-import {getCars} from './controller/controller.js'
+// backend/app.ts
+import express, { Request, Response, NextFunction } from 'express';
+import { getCars } from './controller/controller.js'; // Correct relative path
 import cors from 'cors';
+import path from 'path';
+
 const app = express();
 
-app.use(cors()); //cross origin resource sharing, this will allow my application to communicate between backend and frontend
+app.use(cors());
 app.use(express.json());
 
+// --- API Routes ---
+app.get('/cars', getCars);
 
-app.get('/cars', getCars)
+// --- Serve Static Files (Frontend) ---
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../frontend/public'))); // Serve from frontend/public
 
 
-app.all('/*', (req: Request, res: Response) => {
-  res.status(404).send({ msg: 'Item does not exist' });
+// --- Catch-all Route for SPA ---
+app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html')); // Serve index.html
 });
+
+// --- Error Handling ---
+app.all('/*', (req: Request, res: Response) => {
+    res.status(404).send({ msg: 'Item does not exist' });
+  });
 
 // PostgreSQL error handling
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   if (err.code === '22P02') {
-    res.status(400).send({ msg: "Invalid type (type is wrong)" });
+   res.status(400).send({ msg: "Invalid type (type is wrong)" });
   } else if (err.code === '22003') {
-    res.status(404).send({ msg: "Item does not exist, psql (error 22003)" });
+   res.status(404).send({ msg: "Item does not exist, psql (error 22003)" });
   } else if (err.code === '23502') {
-    res.status(404).send({ msg: "Item does not exist (error 23502)" });
+   res.status(404).send({ msg: "Item does not exist (error 23502)" });
   } else {
-    next(err);
+   next(err);
   }
 });
 
 // server error handling middleware
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err); // Log the error to the console
   res.status(500).send({ msg: "Something went wrong" });
 });
-  
+
 export default app;
