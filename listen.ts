12845d7a12be8e6d42aea5f2f3fb4b2f8bdbea0b
Hm255 <hamza.moosa@outlook.com>
2025-06-12 17:01:58 +0100

    dotenv variables added directly to listen.ts

--- listen.ts
+++ listen.ts
@@ -1,10 +1,9 @@
 import dotenv from "dotenv";
-
+import app from "./app.js";
 dotenv.config({
   path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
 });
 
-import app from "./app.js";
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
