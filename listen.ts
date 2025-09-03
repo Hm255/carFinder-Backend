119b8164c7c1ad2c90bc2d5a79f170e4797986ff
Hm255 <hamza.moosa@outlook.com>
2025-09-03 14:38:42 +0100

    adjustment of package.json and listen.js/ts

--- listen.ts
+++ listen.ts
@@ -1,9 +1,21 @@
 import dotenv from "dotenv";
+import app from "./app.js";
+
 dotenv.config({
   path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
 });
-import app from "./app.js";
 
 const { PORT = 9090 } = process.env;
+app.listen(PORT, () =>
+  console.log(
+    `Listening on ${PORT}... ${new Date().toISOString()} ${
+      process.env.NODE_ENV
+    }`
+  )
+);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+console.log(
+  `${new Date().toISOString()} ${process.env.NODE_ENV} ${
+    process.env.PORT || PORT
+  }`
+);
