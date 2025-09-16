c5ecd2df3755e151de45f45e19681ab0fa661909
Hm255 <hamza.moosa@outlook.com>
2025-09-16 15:41:18 +0100

    origin update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const allowedOrigins = [
     'http://localhost:5173',
     'https://my-only-carfinder.netlify.app/',
-    'https://carfinder-backend.onrender.com/'
+    'https://carfinder-backend.onrender.com'
 ];
 app.use(cors({
     origin: (origin, callback) => {
--- app.ts
+++ app.ts
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'https://my-only-carfinder.netlify.app/', 
-  'https://carfinder-backend.onrender.com/'
+  'https://carfinder-backend.onrender.com'
 ];
 
 app.use(cors({
